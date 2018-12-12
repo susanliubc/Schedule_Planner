c36115cf74288dc9bcca0e8bf2e1085cb1cd4084
susanliubc <susanliubc@gmail.com>
2018-12-11 18:44:43 -0800

    add package.json, App.js, index.js

---
+++ planner/src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Dashboard from './components/dashboard/Dashboard';
+import ProjectDetails from'./components/project/ProjectDetails';
+import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
+import CreateProject from './components/project/CreateProject';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={Dashboard} />
+            <Route path='/project/:id' component={ProjectDetails} />
+            <Route path='/signin' component={SignIn} />
+            <Route path='./signup' component={SignUp} />
+            <Route path='./create' component={CreateProject} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
