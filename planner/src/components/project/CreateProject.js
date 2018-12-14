56cf6ca9f80294d983c40a83db6f666938e8f092
susanliubc <susanliubc@gmail.com>
2018-12-14 13:04:35 -0800

    update components folder

---
+++ planner/src/components/project/CreateProject.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { createProject } from '../../store/actions/projectAction';
+import { Redirect } from 'react-router-dom';
+
+class CreateProject extends Component {
+  state = {
+    title: '',
+    content: ''
+  };
+  handleChange = (e) => {
+    this.setState({
+      [e.target.id]: e.target.value
+    })
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.createProject(this.state);
+    this.props.history.push('/');
+  }
+  render() {
+    const { auth } = this.props;
+    if(!auth.uid) return <Redirect to='/signin' />
+    return (
+      <div className='container'>
+        <form className="white" onSubmit={this.handleSubmit}>
+          <h5 className='grey-text text-darken-3'>Create a new project</h5>
+          <div className="input-field">
+            <label htmlFor="title">Project title</label>
+            <input type="text" id='title' onChange={this.handleChange} />
+          </div>
+          <div className="input-field">
+            <label htmlFor="content">Project content</label>
+            <textarea id='content' className='materialize-textarea' onChange={this.handleChange}></textarea>
+          </div>
+          <div className="input-field">
+            <button className="btn teal lighten-1">Create</button>
+          </div>
+        </form>
+      </div>
+    )
+  }
+};
+
+const mapStateToProps = (state) => {
+  return {
+    auth: state.firebase.auth
+  }
+} 
+
+const mapDispatchToProps = dispatch => {
+    return {
+      createProject: (project) => dispatch(createProject(project))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CreateProject);
