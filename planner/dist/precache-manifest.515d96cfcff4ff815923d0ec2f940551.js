5562d2bbcfe6d7147d4f6768452d6c10f76827bc
susanliubc <susanliubc@gmail.com>
2018-12-14 13:15:03 -0800

    add functions, dist folder

---
+++ planner/dist/precache-manifest.515d96cfcff4ff815923d0ec2f940551.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "636cc0d6502a2c1bb5ad",
+    "url": "/static/js/main.636cc0d6.chunk.js"
+  },
+  {
+    "revision": "5afc6b67a784d44123f5",
+    "url": "/static/js/1.5afc6b67.chunk.js"
+  },
+  {
+    "revision": "636cc0d6502a2c1bb5ad",
+    "url": "/static/css/main.37f46a87.chunk.css"
+  },
+  {
+    "revision": "11d06087896cef2bd7729c8bb52e9f5e",
+    "url": "/index.html"
+  }
+];
